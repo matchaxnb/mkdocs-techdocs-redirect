0551e0e07f5bb2c58431a2f7a7fede09c1dd892a
Matcha @sennder <matcha.desoutter@sennder.com>
2022-11-18 22:10:07 +0100

    fixup! style(black): apply black and autoflake to the code

--- techdocs_redirects/plugin.py
+++ techdocs_redirects/plugin.py
@@ -11,20 +11,26 @@ from mkdocs.plugins import BasePlugin
 def create_or_update_techdocs_metadata(site_dir, extra_data):
     metadata = None
     try:
-        with open(f'{site_dir}/techdocs_metadata.json', 'r', encoding='utf-8') as fh:
+        with open(f"{site_dir}/techdocs_metadata.json", "r", encoding="utf-8") as fh:
             metadata = json.load(fh)
     except FileNotFoundError:
         metadata = {}
     metadata.update(extra_data)
-    with open(f'{site_dir}/techdocs_metadata.json', 'w', encoding='utf-8') as fh:
+    with open(f"{site_dir}/techdocs_metadata.json", "w", encoding="utf-8") as fh:
         json.dump(metadata, fh)
+
 
 class RedirectPlugin(BasePlugin):
     # Any options that this plugin supplies should go here.
     config_scheme = (
-        ('redirect_maps', config_options.Type(dict, default={})),  # note the trailing comma
+        (
+            "redirect_maps",
+            config_options.Type(dict, default={}),
+        ),  # note the trailing comma
     )
 
     # Create TechDocs metadata in the metadata file
     def on_post_build(self, config, **kwargs):
-        create_or_update_techdocs_metadata(config['site_dir'], {'redirects': self.redirects})
+        create_or_update_techdocs_metadata(
+            config["site_dir"], {"redirects": self.redirects}
+        )
